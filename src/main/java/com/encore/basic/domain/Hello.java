d63a88f2dbb1071ce11e9063af2e594920898076
sejongPark <lifedesigner88@gmail.com>
2024-01-23 21:46:24 +0900

    2024 01 23 home

--- src/main/java/com/encore/basic/domain/Hello.java
+++ src/main/java/com/encore/basic/domain/Hello.java
@@ -9,7 +9,10 @@ import lombok.NoArgsConstructor;
 
 //@Getter
 //@Setter
-@Data // getter, setter, 및 toString, equals등 사전 구현
+// getter, setter, 및 toString, equals등 사전 구현
+
+
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Hello {
@@ -29,35 +32,17 @@ public class Hello {
         this.password = myBuilder.password;
     }
 
-    public static MyBuilder builder(){
-        return new MyBuilder();
-    }
+    public static MyBuilder builder(){return new MyBuilder();}
 
     public static class MyBuilder {
-
         private String name;
         private String email;
         private String password;
 
-        public MyBuilder name(String name) {
-            this.name = name;
-            return this;
-        }
+        public MyBuilder name(String name) {this.name = name;return this;}
+        public MyBuilder email(String email) {this.email = email;return this;}
+        public MyBuilder password(String password) {this.password = password;return this;}
 
-        public MyBuilder email(String email) {
-            this.email = email;
-            return this;
-
-        }
-
-        public MyBuilder password(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public Hello build(){
-            return new Hello(this);
-        }
-
+        public Hello build(){return new Hello(this);}
     }
 }
