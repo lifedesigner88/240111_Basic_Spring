30e31ee2fa0a6b04f45160758297c0fd06e4f37e
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2024-01-23 17:49:36 +0900

    2024.01.23 builder(HELLO)

--- src/main/java/com/encore/basic/domain/Hello.java
+++ src/main/java/com/encore/basic/domain/Hello.java
@@ -22,4 +22,42 @@ public class Hello {
         ObjectMapper mapper = new ObjectMapper();
         return  mapper.writeValueAsString(obj);
     }
+
+    public Hello(MyBuilder myBuilder) {
+        this.name = myBuilder.name;
+        this.email = myBuilder.email;
+        this.password = myBuilder.password;
+    }
+
+    public static MyBuilder builder(){
+        return new MyBuilder();
+    }
+
+    public static class MyBuilder {
+
+        private String name;
+        private String email;
+        private String password;
+
+        public MyBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public MyBuilder email(String email) {
+            this.email = email;
+            return this;
+
+        }
+
+        public MyBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Hello build(){
+            return new Hello(this);
+        }
+
+    }
 }
